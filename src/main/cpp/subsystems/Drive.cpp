b41cc31e5b95abef43c0b8650bead69e05f5df44
Collin McIntyre <crm11396@gmail.com>
2022-12-22 23:29:19 -0600

    Add constructor and method templates

--- src/main/cpp/subsystems/Drive.cpp
+++ src/main/cpp/subsystems/Drive.cpp
@@ -1,5 +1,31 @@
 #include "subsystems/Drive.h"
 
-Drive::Drive() = default;
+Drive::Drive() :
+    _frontLeftMotor(1),
+    _backLeftMotor(2),
+    _frontRightMotor(3),
+    _backRightMotor(4)
+{
+    _frontLeftMotor.SetInverted(false);
+    _backLeftMotor.SetInverted(false);
 
-void Drive::Periodic() {}
+    _frontRightMotor.SetInverted(true);
+    _backRightMotor.SetInverted(true);
+
+    _driveMode = DriveMode::Tank;
+}
+
+void Drive::ArcadeDrive(double drive, double rotate)
+{
+
+}
+
+void Drive::TankDrive(double left, double right)
+{
+
+}
+
+void Drive::SetDriveMode(DriveMode mode)
+{
+    
+}
